e49c6bb006a6459a5e839b6de17a0c310b4cc09b
David Pedrick <dpedrick@southface.org>
2019-04-09 18:21:07 -0400

    organizing and print statements
    organize and writing print statements for hwk

--- Q3/hw4q3.py
+++ Q3/hw4q3.py
@@ -37,109 +37,70 @@ x_train,x_test = train_test_split(x_data, test_size = 0.3, shuffle = True, rando
 y_train,y_test = train_test_split(y_data, test_size = 0.3, shuffle = True, random_state=100)
 
 # ############################################### Linear Regression ###################################################
-# XXX
-# TODO: Create a LinearRegression classifier and train it.
-# XXX
 
 lnrRgr = linear_model.LinearRegression()
 lnrRgr.fit(x_train,y_train)
 
+y_pred_train = lnrRgr.predict(x_train).round()
+y_pred_test = lnrRgr.predict(x_test).round()
 
-# XXX
-# TODO: Test its accuracy (on the training set) using the accuracy_score method.
-# TODO: Test its accuracy (on the testing set) using the accuracy_score method.
-# Note: Round the output values greater than or equal to 0.5 to 1 and those less than 0.5 to 0. You can use y_predict.round() or any other method.
-# XXX
-
-y_pred = lnrRgr.predict(x_train)
-y_pred2 = lnrRgr.predict(x_test)
-
-print('Mean squared error: %.2f' % mean_squared_error(y_test, y_pred2))
+#print('Mean squared error: %.2f' % mean_squared_error(y_test, y_pred2))
+print('Linear Regression Accuracy: ')
+print('Linear Regression - Training: %.2f' % accuracy_score(y_train, y_pred_train))
+print('Linear Regression - Test: %.2f' %accuracy_score(y_test, y_pred_test))
 
 
-# ############################################### Random Forest Classifier ##############################################
-# XXX
-# TODO: Create a RandomForestClassifier and train it.
-# X
-
+################################################ Random Forest Classifier ##############################################
 
 clf = RandomForestClassifier(n_estimators=100,max_depth=2,random_state=0)
-
 clf.fit(x_train,y_train)
 
-# XXX
-# TODO: Test its accuracy on the training set using the accuracy_score method.
-# TODO: Test its accuracy on the test set using the accuracy_score method.
-# XXX
+y_pred2_train = clf.predict(x_train)
+y_pred2_test = clf.predict(x_test)
 
-y_pred3_train = clf.predict(x_train)
-y_pred3_test = clf.predict(x_test)
+print('Random Forest Accuracy: ')
+print('Random Forest - Training: %.2f' % accuracy_score(y_train, y_pred2_train))
+print('Random Forest - Training: %.2f' % accuracy_score(y_test, y_pred2_test))
 
-print(accuracy_score(y_train, y_pred3_train))
-print(accuracy_score(y_test, y_pred3_test))
-
-# XXX
-# TODO: Determine the feature importance as evaluated by the Random Forest Classifier.
-#       Sort them in the descending order and print the feature numbers. The report the most important and the least important feature.
-#       Mention the features with the exact names, e.g. X11, X1, etc.
-#       Hint: There is a direct function available in sklearn to achieve this. Also checkout argsort() function in Python.
-# XXX
-
-coefs_list = clf.feature_importances_
-col_names = data.columns.values
-coef_sorted_list = np.argsort(coefs_list)
-coef_sorted_list2 = []
-for i in range(len(data.columns.values)-1):
-    coef_sorted_list2.append(col_names[coef_sorted_list[i]])
-
-print(coef_sorted_list2)
-# XXX
-# TODO: Tune the hyper-parameters 'n_estimators' and 'max_depth'.
-#       Print the best params, using .best_params_, and print the best score, using .best_score_.
-# XXX
-
-
-clf2 = RandomForestClassifier(random_state=0)
-
-n_estimators_list = np.array([10,100])
-max_depth_list = np.array([2,4,8])
-
-grid = GridSearchCV(estimator=clf2, cv=10, param_grid=dict(n_estimators=n_estimators_list, max_depth = max_depth_list))
-
-grid.fit(x_train,y_train)
-
-print(grid.best_score_)
-print(grid.best_estimator_.n_estimators)
-print(grid.best_estimator_.max_depth)
-print(grid.best_params_)
-
-
-# ############################################ Support Vector Machine ###################################################
-# XXX
-# TODO: Pre-process the data to standardize or normalize it, otherwise the grid search will take much longer
-# TODO: Create a SVC classifier and train it.
-# XXX
-
+######################################## SVM ###############################################
 x_normalized = sklearn.preprocessing.normalize(x_data, norm="l2")
 
 x_train_norm,x_test_norm = train_test_split(x_normalized, test_size = 0.3, shuffle = True, random_state=100)
 clf3 = SVC(gamma = "auto", C = 1.0,  kernel = "rbf")
 clf3.fit(x_train_norm, y_train)
 
-# XXX
-# TODO: Test its accuracy on the training set using the accuracy_score method.
-# TODO: Test its accuracy on the test set using the accuracy_score method.
-# XXX
 y_pred4_train = clf3.predict(x_train_norm)
 y_pred4_test = clf3.predict(x_test_norm)
 
-print(accuracy_score(y_test, y_pred4_test))
-print(accuracy_score(y_train, y_pred4_train))
+print('SVM Accuracy: ')
+print('SVM - Test: %.2f' % accuracy_score(y_test, y_pred4_test))
+print('SVM - Training: %.2f' % accuracy_score(y_train, y_pred4_train))
 
-# XXX
-# TODO: Tune the hyper-parameters 'C' and 'kernel' (use rbf and linear).
-#       Print the best params, using .best_params_, and print the best score, using .best_score_.
-# XXX
+
+#################################### Hyperparameter Tuning Q 3.2  #####################################
+print('Q 3.2 Hyperparameter Tuning')
+
+clf2 = RandomForestClassifier(random_state=0)
+n_estimators_list = np.array([10,100])
+max_depth_list = np.array([2,4,8])
+grid = GridSearchCV(estimator=clf2, cv=10, param_grid=dict(n_estimators=n_estimators_list, max_depth = max_depth_list))
+grid.fit(x_train,y_train)
+
+
+print('Random Forest:')
+print('Random Forest - n_estimators: '%n_estimators_list)
+print('Random Forest - max_depth: '% max_depth_list)
+print('Random Forest - best combo: '%best_params_)
+print('Random Forest - best n_estimator: '%grid.best_estimator_.n_estimators)
+print('Random Forest - best max_depth: '%grid.best_estimator_.max_depth)
+
+#### Get accuracy score now #########################################################################
+#### Get accuracy score now #########################################################################
+#### Get accuracy score now #########################################################################
+#### Get accuracy score now #########################################################################
+#### Get accuracy score now #########################################################################
+
+print('SVM:')
 
 c_list = np.array([0.01, 1])
 kernel_list = np.array(["rbf","linear"])
@@ -150,24 +111,44 @@ grid3.fit(x_train_norm, y_train)
 y_pred4_train_grid = grid3.predict(x_train_norm)
 y_pred4_test_grid = grid3.predict(x_test_norm)
 
+print('SVM - Kernel Values tested: '% kernel_list)
+print('SVM - C values tested: '% c_list)
 
-print(accuracy_score(y_test, y_pred4_test_grid))
-print(accuracy_score(y_train, y_pred4_train_grid))
+print('SVM - best kernel: '%grid3.best_estimator_.kernel)
+print('SVM - best C: '%grid3.best_estimator_.C)
 
-print(grid3.best_score_)
-print(grid3.best_params_)
+##################################### Q 3.3 ###################################################################
+########################################### Cross-Validation Results #################################################
 
-# ######################################### Principal Component Analysis #################################################
-# XXX
-# TODO: Perform dimensionality reduction of the data using PCA.
-#       Set parameters n_component to 10 and svd_solver to 'full'. Keep other parameters at their default value.
-#       Print the following arrays:
-#       - Percentage of variance explained by each of the selected components
-#       - The singular values corresponding to each of the selected components.
-# XXX
+print('Q 3.3')
+#print('SVM - Highest mean testing/cv accuracy: '%accuracy_score(y_test, y_pred4_test_grid))
+#print('SVM - Mean train score: '%accuracy_score(y_train, y_pred4_train_grid))
+#print('SVM - Mean fit time: '%accuracy_score(y_train, y_pred4_train_grid))
+
+##################################### Q 3.4 ###################################################################
+#############################  Determine Feature Importance for Random Forest ####################
+
+coefs_list = clf.feature_importances_
+col_names = data.columns.values
+coef_sorted_list = np.argsort(coefs_list) ### Are these in descending order??
+coef_sorted_list2 = []
+for i in range(len(data.columns.values)-1):
+    coef_sorted_list2.append(col_names[coef_sorted_list[i]])
+
+print('Q 3.4')
+print('Random Forest  - Most important feature (e.g. X5): ' % coef_sorted_list2[0])
+print('Random Forest  - Least important feature (e.g. X1): ' % coef_sorted_list2[-1])
+
+##################################### Q 3.5 ###################################################################
+#############################  Determine Feature Importance for Random Forest ####################
+
+#Text written in report.txt file.
+
+##################################### Q 3.6 ###################################################################
+########################################### Principal Component Analysis #################################################
 
 pca = PCA(n_components=10, svd_solver='full')
 pca.fit(x_test)
 
-print(pca.explained_variance_)
-print(pca.singular_values_)
+print('Percent of variance explained: '%pca.explained_variance_)
+print('Singular values corresponding to each component: '%pca.singular_values_)

19aef8095471e4d5ef71b5f3d3353c4a938867f1
David Pedrick <moxypedrick@gmail.com>
2019-04-09 21:00:09 -0400

    finished print statements

--- Q3/hw4q3.py
+++ Q3/hw4q3.py
@@ -15,9 +15,6 @@ from sklearn.decomposition import PCA
 
 
 ######################################### Reading and Splitting the Data ###############################################
-# XXX
-# TODO: Read in all the data. Replace the 'xxx' with the path to the data set.
-# XXX
 
 data = pd.read_csv('C:/Users/dpedrick/OneDrive/GaTech/Data_and_Visual_Analytics/HW4-dpedrick3/hw4-skeleton/Q3/eeg_dataset.csv')
 
@@ -25,14 +22,11 @@ data = pd.read_csv('C:/Users/dpedrick/OneDrive/GaTech/Data_and_Visual_Analytics/
 x_data = data.loc[:, data.columns != "y"]
 y_data = data.loc[:, "y"]
 
-
 # The random state to use while splitting the data.
 random_state = 100
 
-# XXX
 # TODO: Split 70% of the data into training and 30% into test sets. Call them x_train, x_test, y_train and y_test.
-# Use the train_test_split method in sklearn with the parameter 'shuffle' set to true and the 'random_state' set to 100.
-# XXX
+
 x_train,x_test = train_test_split(x_data, test_size = 0.3, shuffle = True, random_state=100)
 y_train,y_test = train_test_split(y_data, test_size = 0.3, shuffle = True, random_state=100)
 
@@ -81,28 +75,27 @@ print('SVM - Training: %.2f' % accuracy_score(y_train, y_pred4_train))
 print('Q 3.2 Hyperparameter Tuning')
 
 clf2 = RandomForestClassifier(random_state=0)
-n_estimators_list = np.array([10,100])
+n_estimators_list = np.array([10,20,100])
 max_depth_list = np.array([2,4,8])
 grid = GridSearchCV(estimator=clf2, cv=10, param_grid=dict(n_estimators=n_estimators_list, max_depth = max_depth_list))
 grid.fit(x_train,y_train)
 
 
 print('Random Forest:')
-print('Random Forest - n_estimators: '%n_estimators_list)
-print('Random Forest - max_depth: '% max_depth_list)
-print('Random Forest - best combo: '%best_params_)
-print('Random Forest - best n_estimator: '%grid.best_estimator_.n_estimators)
-print('Random Forest - best max_depth: '%grid.best_estimator_.max_depth)
+print('Random Forest - n_estimators: '+ str(n_estimators_list))
+print('Random Forest - max_depth: ' + str(max_depth_list))
+print('Random Forest - best n_estimator: ' + str(grid.best_estimator_.n_estimators))
+print('Random Forest - best max_depth: ' + str(grid.best_estimator_.max_depth))
 
-#### Get accuracy score now #########################################################################
-#### Get accuracy score now #########################################################################
-#### Get accuracy score now #########################################################################
-#### Get accuracy score now #########################################################################
-#### Get accuracy score now #########################################################################
+y_pred_random_grid_test = grid.predict(x_test)
+
+print('Random Forest - Hyperparameter Tuning Accuracy')
+print('Random Forest - Before: %.2f' % accuracy_score(y_test, y_pred2_test))
+print('Random Forest - After: %.2f' % accuracy_score(y_test, y_pred_random_grid_test))
 
 print('SVM:')
 
-c_list = np.array([0.01, 1])
+c_list = np.array([0.001, 0.01, 1])
 kernel_list = np.array(["rbf","linear"])
 clf4 = SVC(gamma="auto")
 grid3 = GridSearchCV(estimator = clf4, cv = 10, param_grid = dict(C = c_list, kernel = kernel_list))
@@ -111,19 +104,20 @@ grid3.fit(x_train_norm, y_train)
 y_pred4_train_grid = grid3.predict(x_train_norm)
 y_pred4_test_grid = grid3.predict(x_test_norm)
 
-print('SVM - Kernel Values tested: '% kernel_list)
-print('SVM - C values tested: '% c_list)
+print('SVM - Kernel Values tested: ' + str(kernel_list))
+print('SVM - C values tested: ' + str(c_list))
 
-print('SVM - best kernel: '%grid3.best_estimator_.kernel)
-print('SVM - best C: '%grid3.best_estimator_.C)
+print('SVM - best kernel: ' + str(grid3.best_estimator_.kernel))
+print('SVM - best C: ' + str(grid3.best_estimator_.C))
 
 ##################################### Q 3.3 ###################################################################
 ########################################### Cross-Validation Results #################################################
 
 print('Q 3.3')
-#print('SVM - Highest mean testing/cv accuracy: '%accuracy_score(y_test, y_pred4_test_grid))
-#print('SVM - Mean train score: '%accuracy_score(y_train, y_pred4_train_grid))
-#print('SVM - Mean fit time: '%accuracy_score(y_train, y_pred4_train_grid))
+
+print('SVM - Highest mean testing/cv accuracy:' + str(accuracy_score(y_test,y_pred4_test_grid)))
+print('SVM - Mean train score: ' + str(np.mean(grid3.cv_results_['mean_test_score'])))
+print('SVM - Mean fit time: ' + str(np.mean(grid3.cv_results_['mean_fit_time'])))
 
 ##################################### Q 3.4 ###################################################################
 #############################  Determine Feature Importance for Random Forest ####################
@@ -136,8 +130,8 @@ for i in range(len(data.columns.values)-1):
     coef_sorted_list2.append(col_names[coef_sorted_list[i]])
 
 print('Q 3.4')
-print('Random Forest  - Most important feature (e.g. X5): ' % coef_sorted_list2[0])
-print('Random Forest  - Least important feature (e.g. X1): ' % coef_sorted_list2[-1])
+print('Random Forest  - Most important feature: ' + str(coef_sorted_list2[0]))
+print('Random Forest  - Least important feature: ' + str(coef_sorted_list2[-1]))
 
 ##################################### Q 3.5 ###################################################################
 #############################  Determine Feature Importance for Random Forest ####################
@@ -150,5 +144,5 @@ print('Random Forest  - Least important feature (e.g. X1): ' % coef_sorted_list2
 pca = PCA(n_components=10, svd_solver='full')
 pca.fit(x_test)
 
-print('Percent of variance explained: '%pca.explained_variance_)
-print('Singular values corresponding to each component: '%pca.singular_values_)
+print('Percent of variance explained: ' + str(pca.explained_variance_))
+print('Singular values corresponding to each component: ' + str(pca.singular_values_))
